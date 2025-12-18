5978319e7ae1ef1bfb14e5e9276011be0bda55f0
Youri <yourish@live.nl>
2025-12-18 09:39:31 +0100

    Update to API14 [PUSH]

--- XIVMoneyTracker/Windows/MoneyGraph.cs
+++ XIVMoneyTracker/Windows/MoneyGraph.cs
@@ -15,7 +15,7 @@ namespace FFXIVMoneyTracker.Windows
 
         public override void Draw()
         {
-            if (!Visible)
+            if (true) //!Visible)
             {
                 return;
             }
@@ -25,38 +25,70 @@ namespace FFXIVMoneyTracker.Windows
                 ImGui.Text("No character loaded");
                 return;
             }
-
-            int average = (int)plugin.CurrentCharacter.Transactions.Average(x => x.Total);
-            string unitName = "gil";
-            float divisionFactor = 1;
-            if (average > 1000 && average < 1000000)
+            try
             {
-                divisionFactor = 1000;
-                unitName = "thousand gil";
-            }
-            if (average > 1000000)
-            {
-                divisionFactor = 1000000;
-                unitName = "million gil";
-            }
-
-
-            ImGui.SetNextWindowSize(new Vector2(500, 500), ImGuiCond.FirstUseEver);
-            if (ImGui.Begin("Money graph", ref this.visible))
-            {
-                if (ImGui.InputInt("Amount of days shown", ref daysShown, 5, 30, default, ImGuiInputTextFlags.EnterReturnsTrue))
+                int average = (int)plugin.CurrentCharacter.Transactions.Average(x => x.Total);
+                string unitName = "gil";
+                float divisionFactor = 1;
+                if (average > 1000 && average < 1000000)
                 {
+                    divisionFactor = 1000;
+                    unitName = "thousand gil";
+                }
+                if (average > 1000000)
+                {
+                    divisionFactor = 1000000;
+                    unitName = "million gil";
                 }
 
-                Vector2 childScale = new Vector2(ImGui.GetWindowWidth() - 15, ImGui.GetWindowHeight() - 100);
 
-                float[] graphData = plugin.CurrentCharacter.Transactions
-                    .Where(x => x.TimeStamp > DateTime.Now.AddDays(-daysShown))
-                    .Select(x => (float)Math.Round(x.Total / divisionFactor, 3)).ToArray();
+                ImGui.SetNextWindowSize(new Vector2(500, 500), ImGuiCond.FirstUseEver);
+                if (ImGui.Begin("Money graph", ref this.visible))
+                {
 
-                ImGui.PlotHistogram(ImU8String.Empty, graphData, 0, new ImU8String(unitName), float.MaxValue, float.MaxValue, childScale);
+                    ImGui.InputInt("Amount of days shown", ref daysShown, 5, 30, default, ImGuiInputTextFlags.EnterReturnsTrue);
+
+
+                    Vector2 childScale = new Vector2(ImGui.GetWindowWidth() - 15, ImGui.GetWindowHeight() - 100);
+
+                    var test1 = plugin.CurrentCharacter.Transactions.ToArray();
+                    var test2 = plugin.CurrentCharacter.Transactions.Where(x => x.TimeStamp > DateTime.Now.AddDays(-daysShown)).ToArray();
+
+
+                    float[] graphData = plugin.CurrentCharacter.Transactions
+                        .Where(x => x.TimeStamp > DateTime.Now.AddDays(-daysShown))
+                        .Select(x => (float)Math.Round(x.Total / divisionFactor, 3)).ToArray();
+
+                    if (graphData.Length > 1 && childScale.X > 0 && childScale.Y > 0)
+                    {
+                        var overlay = new ImU8String(unitName);
+                        ImGui.PlotHistogram(
+                            ImU8String.Empty,
+                            graphData,
+                            0,
+                            overlay,
+                            float.MaxValue,
+                            float.MaxValue,
+                            childScale
+                        );
+                    }
+                    else
+                    {
+                        ImGui.Text("Not enough data points or invalid graph size to show a graph. Please wait until more data is collected.");
+                    }
+                }
+                ImGui.End();
             }
-            ImGui.End();
+            catch (Exception e)
+            {
+                if (ImGui.Begin("Money graph", ref this.visible))
+                {
+                    ImGui.Text("Error drawing graph:");
+                    ImGui.Text(e.Message);
+                }
+
+
+            }
         }
     }
 }
